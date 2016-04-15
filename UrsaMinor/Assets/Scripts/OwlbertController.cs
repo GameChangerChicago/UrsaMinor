e92946d6bae65ba8b8047d18e63bbec4f0f06e1e
mwarrington <mason.arrington@gmail.com>
2016-04-15 16:02:23 -0500

    Start of Owlbert

---
+++ UrsaMinor/Assets/Scripts/OwlbertController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OwlbertController : MovementController
+{
+    public List<TravelNode> TravelNodes;
+
+    private Vector2 _currentTarget;
+
+    private void SetTarget()
+    {
+    //chea
+    }
+
+    private void MoveToNode()
+    {
+
+    }
+}
---
+++ UrsaMinor/Assets/Scripts/TravelNode.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TravelNode : MonoBehaviour
+{
+    public float VerticalRange;
+}
