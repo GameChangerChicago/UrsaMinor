2e9446283971227357d2cee8bda74e5334daeea7
kaira2006 <kayura60@gmail.com>
2016-04-22 14:19:45 -0500

    Level 1 Sync

---
+++ UrsaMinor/Assets/MovablePlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovablePlatform : MonoBehaviour
+{
+
+
+	public float speed;
+	private Vector3 platformVector;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		platformVector = GetComponent<Transform> ().position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+
+	}
+
+	void MovePlatform ()
+	{
+
+
+	}
+}
